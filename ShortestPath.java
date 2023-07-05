20b7e11d945ed110cff200391a1a8d83f0d02a0e
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2023-07-05 16:13:34 +0530

    Add files via upload

---
+++ ShortestPath.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ShortestPath {
+    public static void shortestpath(String direction) {
+        int x = 0, y = 0;
+        for (int i = 0; i < direction.length(); i++) {
+            char next_dir = direction.charAt(i);
+            if (next_dir == 'N') {
+                y++;
+            } else if (next_dir == 'E') {
+                x++;
+            } else if (next_dir == 'S') {
+                y--;
+            } else {
+                x--;
+            }
+        }
+        double shortest_path = Math.sqrt((x * x + y * y));
+        System.out.println("Final position of the person is: " + x + " " + y);
+        System.out.println("Shortest Path: " + shortest_path);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter directions to be followed: ");
+        String direction = sc.next();
+        shortestpath(direction);
+        sc.close();
+
+    }
+
+}
